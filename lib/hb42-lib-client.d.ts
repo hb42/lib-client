d43526b204a7fc89ec0f4973d1065d2e4f8f3d11
Hartmut Bauer <hb42@gmx.de>
2019-04-27 18:48:40 +0200

    publish npm package

---
+++ lib/hb42-lib-client.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './index';
+export { FlexboxSplitter as ɵb } from './component';
+export { initErrorHandler as ɵa } from './module';
+export { FileSizePipe as ɵc, IEDatePipe as ɵd } from './pipe';
+export { JwtHelperService as ɵf, LogonInterceptor as ɵg, LogonService as ɵe } from './service/logon';
--- lib/src/override/xhrbackend.handler.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Created by hb on 07.08.16.
- *
- * HTTP-Errors abfangen und withCredentials setzen
- *
- * s. http://stackoverflow.com/questions/34934009/handling-401s-globally-with-angular-2
- *
- * *** Fuer den Fall, dass so etwas mal gebraucht wird ... ***
- */
