1952184d1d1c595f84f085905e7089d36dead663
Hartmut Bauer <hb42@gmx.de>
2020-02-07 02:03:11 +0100

    fix

--- src/module.ts
+++ src/module.ts
@@ -2,10 +2,15 @@ import { CommonModule } from "@angular/common";
 import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { ErrorHandler, NgModule } from "@angular/core";
 
-import { FlexboxSplitter } from "./component";
-import { FileSizePipe, IEDatePipe } from "./pipe";
-import { ElectronService, ErrorService, VersionService } from "./service";
-import { JwtHelperService, LogonInterceptor, LogonService } from "./service/logon";
+import { FlexboxSplitter } from "./component/splitter.directive";
+import { FileSizePipe } from "./pipe/filesize.pipe";
+import { IEDatePipe } from "./pipe/iedate.pipe";
+import { ElectronService } from "./service/electron.service";
+import { ErrorService } from "./service/error.service";
+import { JwtHelperService } from "./service/logon/jwt-helper.service";
+import { LogonInterceptor } from "./service/logon/logon-interceptor";
+import { LogonService } from "./service/logon/logon.service";
+import { VersionService } from "./service/version.service";
 
 // Verhindern, dass ErrorService mehrfach instanziiert wird
 export function initErrorHandler(errorService: ErrorService) {
