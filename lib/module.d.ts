541a9cc203fc1435d09379cc9c4eb418ff8bf6ee
Hartmut Bauer <hb42@gmx.de>
2020-02-09 18:16:59 +0100

    Build-Logik aendern

--- lib/module.d.ts
+++
@@ -1,4 +0,0 @@
-import { ErrorService } from "./service/error.service";
-export declare function initErrorHandler(errorService: ErrorService): ErrorService;
-export declare class LibClientModule {
-}
--- lib/pipe/filesize.pipe.d.ts
+++
@@ -1,7 +0,0 @@
-import { PipeTransform } from "@angular/core";
-export declare class FileSizePipe implements PipeTransform {
-    private readonly suffix;
-    constructor();
-    transform(value: any): any;
-    private conv;
-}
